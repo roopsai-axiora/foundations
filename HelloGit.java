f0948e57b1073390de36d8ead6943f1c87ead187
roopsai-axiora <roopsai.s@axioraglobalsolutions.com>
2026-03-11 17:50:02 +0530

    fix(src): improve greeting message in HelloGit

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Learning Git");
+        System.out.println("Learning Git — Conventional Commits!");
+
     }
 }
