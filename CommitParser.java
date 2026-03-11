389194b9f5df60381cd4fffbb41a812eac2e6f65
roopsai-axiora <roopsai.s@axioraglobalsolutions.com>
2026-03-11 18:04:55 +0530

    feat(git): add CommitParser for conventional commit messages

---
+++ CommitParser.java
@@ -0,0 +1,23 @@
+public class CommitParser {
+    public String[] parse(String message) {
+        // expected: type(scope): description
+        int colon = message.indexOf(":");
+        String left = message.substring(0, colon).trim();
+        String desc = message.substring(colon + 1).trim();
+
+        String type;
+        String scope = "";
+
+        int open = left.indexOf("(");
+        int close = left.indexOf(")");
+
+        if (open > 0 && close > open) {
+            type = left.substring(0, open);
+            scope = left.substring(open + 1, close);
+        } else {
+            type = left;
+        }
+
+        return new String[] { type, scope, desc };
+    }
+}
